200c5fb47575fec5078dab6f64f1f78479195af4
Pete R <peachananr@gmail.com>
2013-11-06 09:59:17 +0700

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-interactive_3d
-==============
-
+#Interactive 3D by Pete R.
 Create a 3D interactive object using images and one simple JS call
+Created by [Pete R.](http://www.thepetedesign.com), Founder of [BucketListly](http://www.bucketlistly.com)
+
+[![Interactive 3D](http://www.thepetedesign.com/images/interactive_3d.png "Interactive 3D")](http://www.thepetedesign.com/demos/interactive_3d.html)
+
+## Demo
+[View demo](http://www.thepetedesign.com/demos/interactive_3d.html)
+
+## Compatibility
+Modern browsers such as Chrome, Firefox, and Safari on both desktop and smartphones have been tested. Not tested on IE.
+
+## Basic Usage
+jQuery Interactive 3D let you create a 3D interactive object using several images as frames allowing the user to drag around and manipulate the image. This plugin is perfect for showcasing your product on the web.
+
+To add this to your website, simply include the latest jQuery library together with `jquery.interactive_3d.js` into your document's `<head>`, create a markup and call the function as follows:
+
+````html
+<body>
+  ...
+  <div id="interactive_3d">
+    <img src="images/frame_1.png">
+  </div>
+  ...
+</body>
+````
+Note: Make sure that the images that will be used as frames have a file name that ends with `_frame-number`. For example, your first frame should be named: `anything_1.jpg` and your second should be `anything_2.jpg` etc.
+
+Now call the function and that should be it.
+ 
+````javascript
+  $("#interactive_3d").interactive_3d({
+    frames: 10, // The total number of images to be used as frames. The higher, the smoother your interaction will be. The default value is 10 frames.
+    cursor: "move", // The CSS style to indicate what cursor will show when the user hover the object. The default value is "move"
+    speed: 0, // The speed of the rotation in milliseconds delay. If you have small number of frames and the rotation seems too fast and not smooth, increase this value to 50 - 100 milliseconds delay. The default value is 0.
+    entrance: true, // Entrance Animation. Toggle this to false to turn it off. The default value is true.
+    preloadImages: true, // Let the script preload all the frames on initial load. Toggle this to false to turn it off. The default value is true.
+    touchSupport: true, // The script support touch events for mobile phones. If this interferes with your website behaviour, you can toggle this to false. The default value is true.
+    loading: "Loading.." // This only applies if preloadImages is true. This option let you show a loading indicator while the script is preloading the images. The option accepts HTML. Toggle this to false to turn this off. The default value is "Loading.."
+  });
+````
+
+With one JS call, you will now have an awesome 3D interactive images on your website. Pretty neat huh? A big shout out to [Chris Coyier](http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/) for providing a lean solution for drag and drop without using jQuery UI and thanks to the owner of touchHandler script (not sure who the author was. The original post was on a lost Posterous server) to add touch support to the drag and drop script.
+
+If you want to see more of my plugins, visit [The Pete Design](http://www.thepetedesign.com/#design), or follow me on [Twitter](http://www.twitter.com/peachananr) and [Github](http://www.github.com/peachananr).
+
+## Other Resources
+- [Chris Coyier's drag and drop script](http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/)
+- [TouchHandler script](http://stackoverflow.com/questions/5186441/javascript-drag-and-drop-for-touch-devices)
---
+++ demo/jquery.interactive_3d.js
@@ -0,0 +1,206 @@
+/* ===========================================================
+ * jquery.interactive_3d.js v1
+ * ===========================================================
+ * Copyright 2013 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * Create a 3D interactive object using images as frames
+ * with one js call
+ *
+ * https://github.com/peachananr/interactive_3d
+ *
+ * ========================================================== */
+
+!function($){
+  
+  var defaults = {
+    frames: 10,
+    cursor: "move",
+    speed: 0,
+    entrance: true,
+    preloadImages: true,
+    touchSupport: true,
+    loading: "Loading.."
+	};
+	
+	function touchHandler(event) {
+      var touch = event.changedTouches[0];
+
+      var simulatedEvent = document.createEvent("MouseEvent");
+          simulatedEvent.initMouseEvent({
+          touchstart: "mousedown",
+          touchmove: "mousemove",
+          touchend: "mouseup"
+      }[event.type], true, true, window, 1,
+          touch.screenX, touch.screenY,
+          touch.clientX, touch.clientY, false,
+          false, false, false, 0, null);
+
+      touch.target.dispatchEvent(simulatedEvent);
+  }
+	
+	$.fn.preload = function(el) {
+	  $("<div class='images_cache'></div>").hide().appendTo(el);
+    this.each(function(){
+        $('<img/>').attr("src", this).appendTo(".images_cache")
+    });
+  }
+	
+	$.fn.drags = function(settings) {
+
+    var $el = this;
+    
+    return $el.css('cursor', settings.cursor).on("mousedown", function(e) {
+        var $drag = $(this).addClass('draggable'),
+            cur_pos = e.pageX,
+            last_position = {};           
+        
+        $drag.parents().on("mousemove", function(e) {
+            if($('.draggable').length > 0) {
+              var src = $el.find("img.main-frame").attr("src"),
+                  img_name = src.split('/')[src.split('/').length-1],
+                  cur_frame = img_name.split('_')[1].split('.')[0];
+                  
+              if (typeof(last_position.x) != 'undefined') {
+                //get the change from last position to this position
+                var deltaX = last_position.x - e.clientX,
+                    deltaY = last_position.y - e.clientY;
+                
+                if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
+                  if(cur_frame > 1) {
+                    setTimeout(function() {
+                       var img_name = src.split('/')[src.split('/').length-1]
+                       var directory = src.split('/').slice(0, -1).join("/")
+                       var new_frame = directory + "/" + img_name.split('_')[0] + "_" + (parseInt(cur_frame) - 1) + "." + img_name.split('.')[1]
+                        $el.find("img.main-frame").attr("src", new_frame)
+                    },settings.speed)
+                  } else {
+                    setTimeout(function() {
+                      var img_name = src.split('/')[src.split('/').length-1]
+                      var directory = src.split('/').slice(0, -1).join("/")
+                      var new_frame = directory + "/" + img_name.split('_')[0] + "_" + (parseInt(settings.frames)) + "." + img_name.split('.')[1]
+                      $el.find("img.main-frame").attr("src", new_frame)
+                    },settings.speed)
+                   
+                  }
+                } else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
+                  if(cur_frame < settings.frames) {
+                    setTimeout(function() {
+                      var img_name = src.split('/')[src.split('/').length-1]
+                      var directory = src.split('/').slice(0, -1).join("/")
+                      var new_frame = directory + "/" + img_name.split('_')[0] + "_" + (parseInt(cur_frame) + 1) + "." + img_name.split('.')[1]
+                        $el.find("img.main-frame").attr("src", new_frame)
+                    },settings.speed)
+                  } else {
+                    setTimeout(function() {
+                      var img_name = src.split('/')[src.split('/').length-1]
+                      var directory = src.split('/').slice(0, -1).join("/")
+                      var new_frame = directory + "/" + img_name.split('_')[0] + "_" + 1 + "." + img_name.split('.')[1]
+                      $el.find("img.main-frame").attr("src", new_frame)
+                    },settings.speed)
+                  }
+                }
+              }    
+                  
+              last_position = {
+                  x : e.clientX,
+                  y : e.clientY
+              };
+            }
+            $(".draggable").on("mouseup", function() {
+                $(this).removeClass('draggable')
+            });
+        });
+        e.preventDefault(); // disable selection
+    }).on("mouseup", function() {
+      $(this).removeClass('draggable');
+    });
+  }
+	
+
+  $.fn.interactive_3d = function(options){
+    var settings = $.extend({}, defaults, options),
+        el = $(this);
+    el.find(" > img").addClass("main-frame");  
+    el.drags(settings);
+    if (settings.entrance == true) {
+      var cur_frame = el.find("img.main-frame").attr("src").split('_')[1].split('.')[0];
+      
+      var x = 0,
+          step = 100 / parseInt(settings.frames);
+
+      function animate_3d() {
+        var src = el.find("img.main-frame").attr("src");
+        el.find("img.main-frame").css("opacity", (x * step)/100);
+        if(cur_frame < settings.frames) {
+          setTimeout(function() {
+            var img_name = src.split('/')[src.split('/').length-1]
+            var directory = src.split('/').slice(0, -1).join("/")
+            var new_frame = directory + "/" + img_name.split('_')[0] + "_" + (parseInt(cur_frame) + 1) + "." + img_name.split('.')[1]
+            el.find("img.main-frame").attr("src", new_frame)
+            cur_frame = parseInt(cur_frame) + 1;
+          },settings.speed)
+        } else {
+          setTimeout(function() {
+            var img_name = src.split('/')[src.split('/').length-1]
+            var directory = src.split('/').slice(0, -1).join("/")
+            var new_frame = directory + "/" + img_name.split('_')[0] + "_" + 1 + "." + img_name.split('.')[1]
+            el.find("img.main-frame").attr("src", new_frame)
+            cur_frame = 1;
+          },settings.speed)
+        }
+        
+        if (x++ < (settings.frames - 1)) {
+            setTimeout(animate_3d,  (x * 1.5));
+        }
+      }
+      
+      
+      if (settings.loading == false) animate_3d(); 
+    }
+    
+    if (settings.touchSupport == true) {
+      document.addEventListener("touchstart", touchHandler, true);
+      document.addEventListener("touchmove", touchHandler, true);
+      document.addEventListener("touchend", touchHandler, true);
+      document.addEventListener("touchcancel", touchHandler, true);
+    }
+    
+    
+    
+    if (settings.preloadImages == true) {
+      var src = el.find("img.main-frame").attr("src");
+      arr = []
+      for (var i = 1; i < settings.frames + 1; i++) {
+        var img_name = src.split('/')[src.split('/').length-1]
+        var directory = src.split('/').slice(0, -1).join("/")
+        arr.push(directory + "/" + img_name.split('_')[0] + "_" + i + "." + img_name.split('.')[1])
+      }
+      $(arr).preload(el);
+      
+      if (settings.loading != false) {
+        var imgs = $(".images_cache > img").not(function() { return this.complete; });
+        var count = imgs.length;
+        el.append("<div class='loading_3d'>" + settings.loading + "</div>");
+        el.find(".main-frame").css("visibility", "hidden");
+        if (count) {
+            imgs.load(function() {
+                count--;
+                if (!count) {
+                    el.find(".main-frame").css("visibility", "visible");
+                    el.find(".loading_3d").remove();
+                    animate_3d();
+                }
+            });
+        } else {
+          el.find(".main-frame").css("visibility", "visible");
+          el.find(".loading_3d").remove();
+          animate_3d();
+        }
+      }
+      
+    }
+    
+  }
+}(window.jQuery);
+
